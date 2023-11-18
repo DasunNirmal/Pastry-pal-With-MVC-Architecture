505285265ba56fb4763f508cfd19ed5dd5d3217f
DasunNirmal <dasun200301@gmail.com>
2023-11-18 21:36:32 +0530

    RegEx to register form

--- src/main/java/lk/ijse/PastryPal/RegExPatterns/RegExPatterns.java
+++ src/main/java/lk/ijse/PastryPal/RegExPatterns/RegExPatterns.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class RegExPatterns {
     @Getter
     public static final Pattern validName = Pattern.compile("\\b[A-Z][a-z]*( [A-Z][a-z]*)*\\b");
+    @Getter
     public static final Pattern validPassword = Pattern.compile("(.*?[0-9]){4,}");
-
 
 }
